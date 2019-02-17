71da499f8d6d27cb75e3ebd40097431f6c74ffd1
SeonHyungJo <seonhyung.jo@gmail.com>
2019-02-18 00:15:41 +0900

    :+1: Modified useCallback, useMemo, useLayoutEffect, useEffect

--- src/useExamle/DiffMemo.js
+++ src/useExamle/DiffMemo.js
@@ -1,22 +1,38 @@
-import React, { useEffect, useState, useMemo } from 'react'
+import React, { useEffect, useState, useMemo, useCallback, useLayoutEffect } from 'react'
 
 const DiffMemo = ({ list }) => {
   const [count, setCount] = useState(0)
+  // 일반적으로 사용하게 되면 ReRender시에 무족건 작동합니다.
+  const normalSort = list.sort((a, b) => a - b)
+
+  // useCallback과 useMemo와 비슷한 듯 비슷하지 않음
+  // useCallback과 useMemo의 순서를 굳이 따지자면 소스를 적는 순서이다.
+  useCallback(
+    console.log('useCallback'),
+    [count]
+  )
 
   // Memoized
+  // useCallback과 useMemo와 비슷하게 흘러감 & 최적화에 사용이 될 듯함
   const sort = useMemo(
     () => {
       console.log('useMemo')
       return list.sort((a, b) => a - b)
     },
-    [list] // 다음과 같이 list의 변화가 일어날 때만 렌더하고 변화가 없으면 기억된 내용을 이용
+    [count] // 다음과 같이 list의 변화가 일어날 때만 렌더하고 변화가 없으면 기억된 내용을 이용
   )
 
-  // 일반적으로 사용하게 되면 ReRender시에 무족건 작동합니다.
-  const normalSort = list.sort((a, b) => a - b)
+  // Redering은 되었고 paint, flow가 되지 않은 상태
+  useLayoutEffect(
+    () => {
+      console.log('useLayoutEffect')
+    },
+    [count]
+  )
 
+  // paint, flow가 된 상태
   useEffect(() => {
-    console.log('re-render')
+    console.log('useEffect')
   })
 
   return (
